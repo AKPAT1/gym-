20b54bc355d5d35e09ad551f623fff383af35b3e
AKPAT <a.kaveiani@gmail.com>
2024-07-21 03:28:59 +0330

    Honors

--- gym.js
+++ gym.js
@@ -19,3 +19,19 @@ $(window).scroll(function()
             $('.nav-menu').removeClass('cu-navbar')
         }
 })
+
+$(window).scroll(function()
+{
+    let pos = $(this).scrollTop();
+    console.log(pos)
+    if(pos >= 700 )
+        {
+            $('.honors-text').addClass('fromleft')
+            $('.honors-img').addClass('fromright')
+        }
+        else
+        {
+            $('.honors-text').removeClass('fromleft')
+            $('.honors-img').removeClass('fromright')
+        }
+})
